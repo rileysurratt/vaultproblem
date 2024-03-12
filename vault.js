bf22d6e7fc1a928b2965c9e55dc7c8a9442d8c95
rileysurratt <rileysurratt13@gmail.com>
2024-03-11 21:19:03 -0400

    vault problem 1st commit

---
+++ vault.js
@@ -0,0 +1,20 @@
+// create message to display to user
+// assign 3 variables using different arithmic operators
+// combination: 10 - 40 - 39
+// create a alert pop up to display message
+
+// assign message to a variable 
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// assign numbers to variables
+const num1 = 5 + 5
+const num2 = 20 * 2
+const num3 = 49 - 10
+
+// create popup dialog boxes w/ alert messages - one message for each number
+alert(message)
+alert("Number 1 is: " + num1)
+alert("Number 2 is: " + num2)
+alert("Number 3 is: " + num3)
+
